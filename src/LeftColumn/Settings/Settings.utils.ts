0e4212eb809d0438d11f3186c87cff74777f4037
Domino731 <d.orzechowski18@gmail.com>
2024-07-18 19:28:54 +0200

    displaying single stage details

---
+++ src/LeftColumn/Settings/Settings.utils.ts
@@ -0,0 +1,13 @@
+export const useSettingsOptions = () => {
+    return [
+        {
+            label: "Hide tiles"
+        },
+        {
+            label: "Hide objects"
+        },
+        {
+            label: "Hide map grid border"
+        }
+    ]
+}
